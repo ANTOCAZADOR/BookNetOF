f37423d21cf555b849afe88eb64274859a519225
ANTOCAZADOR <antonio.brian.gaytan15@gmail.com>
2024-11-06 15:33:09 -0600

    Aquí ya se tiene hasta el archivo de sessions

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\UserController;
-
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,7 +23,6 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
-
 
 Route::resource( 'user', UserController::class)
     ->parameters(['user' => 'user'])

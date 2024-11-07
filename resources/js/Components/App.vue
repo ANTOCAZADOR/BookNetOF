1a8bf56c3fb296cb5f9bc16fd62a12acf70af94f
ANTOCAZADOR <antonio.brian.gaytan15@gmail.com>
2024-11-07 13:39:14 -0600

    Hasta aquí ya funciona todo y se instalo en vue

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,18 @@
+<!-- resources/js/components/App.vue -->
+<template>
+    <div>
+        <h1>Hello, Vue.js in Laravel!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>

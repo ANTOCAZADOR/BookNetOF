e66f0d37331e8d1afbf3cb12060a80b483d4c8d0
AndreaHerrera25 <andrea.herrera6279@alumnos.udg.mx>
2024-11-13 18:46:22 -0600

    Pagina principal diseño

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -5,17 +5,20 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 <template>
     <div>
         <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-            <ApplicationLogo class="block h-12 w-auto" />
+            
+            <img src="/assets/img/kaiadmin/logo_light.png" alt="Logo de la aplicación" class="block h-12 w-auto w-20 h-auto mx-auto my-4" />
 
             <h1 class="mt-8 text-2xl font-medium text-gray-900">
-                Welcome to your Jetstream application!
+            
+                ¡Bienvenido a BookNet, tu sistema integral de gestión bibliotecaria!
             </h1>
 
             <p class="mt-6 text-gray-500 leading-relaxed">
-                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                ecosystem to be a breath of fresh air. We hope you love it.
+                BookNet es una herramienta diseñada para facilitar el acceso y la gestión de recursos bibliográficos, brindando una experiencia 
+                intuitiva tanto a estudiantes y lectores como a administradores de la biblioteca. Aquí podrás consultar la disponibilidad de nuestros
+                libros en tiempo real, realizar reservas y gestionar tus préstamos de manera sencilla y eficiente. Los administradores cuentan con 
+                 funcionalidades avanzadas para organizar el inventario, controlar los préstamos, gestionar categorías y enviar notificaciones a los 
+                 usuarios, todo en un solo lugar.
             </p>
         </div>
 
--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,9 +2,9 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your Jetstream application!
+        Welcome to your BOOKNET application!
     </h1>
-
+    
     <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
         Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
         to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
